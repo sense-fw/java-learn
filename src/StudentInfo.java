308fbdc5e7430d13086f61dd337ee21b69b31140
Darya Sharova <iwch19d@mail.ru>
2026-06-15 17:10:46 +0300

    complete practice 2

---
+++ src/StudentInfo.java
@@ -0,0 +1,15 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Ryan";
+        int age = 23;
+        double averageScore = 4.9;
+        boolean isStudent = true;
+        char group = 'Z';
+
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Средний балл по географии: " + averageScore);
+        System.out.println("Студент: " + isStudent);
+        System.out.println("Группа: " + group);
+    }
+}
